41ed7d1d7e34945b5449a0306003971e71a26be4
김종혁 <enfycius@gimjonghyeog-ui-MacBookPro.local>
2023-11-19 21:44:54 +0900

    fix #1

---
+++ OwnLab/OwnLab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OwnLab
+//
+//  Created by 김종혁 on 11/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
