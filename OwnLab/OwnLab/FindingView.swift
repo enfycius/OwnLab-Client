19e6d7e34bb74067331af068d52bdb1cf0ca2fe8
김종혁 <enfycius@gimjonghyeog-ui-MacBookPro.local>
2023-11-19 23:32:52 +0900

    로그인 화면 UI 작업 완료(iOS) & .gitignore 등록

---
+++ OwnLab/OwnLab/FindingView.swift
@@ -0,0 +1,18 @@
+//
+//  FindingView.swift
+//  OwnLab
+//
+//  Created by 김종혁 on 11/19/23.
+//
+
+import SwiftUI
+
+struct FindingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FindingView()
+}
---
+++ OwnLab/OwnLab/RegisterView.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterView.swift
+//  OwnLab
+//
+//  Created by 김종혁 on 11/19/23.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegisterView()
+}
